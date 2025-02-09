6c222ad6307cb18f1aa5a6037b42163d6839176c
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-02-09 20:13:42 +0900

    Changes to be committed: InvUI API 추가 	modified:   .gitignore 	modified:   build.gradle.kts 	modified:   src/main/java/com/rrayy/msit/msit.java

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,12 @@ repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // Spigot API
     maven("https://oss.sonatype.org/content/repositories/snapshots/") // BungeeCord Chat API
+    maven("https://repo.xenondevs.xyz/releases") // InvUI 라이브러리
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT") // Spigot API
+    compileOnly("xyz.xenondevs.invui:invui:1.44") // InvUI 라이브러리 추가
 }
 
 tasks.withType<JavaCompile>().configureEach {
--- src/main/java/com/rrayy/msit/msit.java
+++ src/main/java/com/rrayy/msit/msit.java
@@ -1,27 +1,30 @@
 package com.rrayy.msit;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import xyz.xenondevs.invui.InvUI;
+import xyz.xenondevs.invui.gui.Gui;
+import xyz.xenondevs.invui.item.builder.ItemBuilder;
+import xyz.xenondevs.invui.item.impl.SimpleItem;
+import xyz.xenondevs.invui.window.Window;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class msit extends JavaPlugin implements Listener {
-    private final Map<Player, Inventory> playerInventories = new HashMap<>();
+    private final Map<Player, Window> playerWindows = new HashMap<>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("msit plugin has been enabled!");
         getServer().getPluginManager().registerEvents(this, this);
+        InvUI.getInstance().setPlugin(this);
+        getLogger().info("msit plugin has been enabled!");
     }
 
     @Override
@@ -35,16 +38,27 @@ public class msit extends JavaPlugin implements Listener {
         Player p = e.getPlayer();
         if (p.isSneaking()) {
             e.setCancelled(true);
-            Inventory inventory = playerInventories.computeIfAbsent(p, k -> Bukkit.createInventory(p, InventoryType.CHEST));
-
-            // 인벤토리의 첫 번째 슬롯에 아이템을 추가합니다.
-            ItemStack item1 = new ItemStack(Material.DIAMOND, 1);
-            ItemStack item2 = new ItemStack(Material.GOLD_INGOT, 1);
-            ItemStack item3 = new ItemStack(Material.IRON_INGOT, 1);
-            inventory.setItem(0, item1);
-            inventory.setItem(1, item2);
-            inventory.setItem(2, item3);
-            p.openInventory(inventory);
+            Window window = playerWindows.computeIfAbsent(p, k -> createInventoryWindow(p));
+            window.open();
         }
+    }
+
+    private Window createInventoryWindow(Player player) {
+        Gui gui = Gui.normal() // Creates the GuiBuilder for a normal GUI
+        .setStructure(
+        "# # # # # # # # #",
+        "# . a . b . c . #",
+        "# . A . B . C . #",
+        "# # # # # # # # #")
+        .addIngredient('#', new SimpleItem(new ItemBuilder(Material.LIGHT_GRAY_STAINED_GLASS_PANE)))
+        .build();
+
+        Window window = Window.single()
+        .setViewer(player)
+        .setTitle("InvUI")
+        .setGui(gui)
+        .build();
+        
+        return window;
     }
 }

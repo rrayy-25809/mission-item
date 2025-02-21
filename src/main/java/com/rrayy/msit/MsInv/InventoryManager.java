875f51d5c286583041d4068142d3dbef0c06ea80
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-02-22 01:28:01 +0900

    Changes to be committed: 조짐 	deleted:    src/main/java/com/rrayy/msit/MsInv/EventListener.java 	modified:   src/main/java/com/rrayy/msit/MsInv/InventoryManager.java

--- src/main/java/com/rrayy/msit/MsInv/InventoryManager.java
+++ src/main/java/com/rrayy/msit/MsInv/InventoryManager.java
@@ -4,8 +4,6 @@ import org.bukkit.entity.Player;
 import com.rrayy.msit.msit;
 
 import xyz.xenondevs.invui.gui.Gui;
-import xyz.xenondevs.invui.item.Item;
-import xyz.xenondevs.invui.item.impl.SimpleItem;
 import xyz.xenondevs.invui.window.Window;
 
 import java.util.HashMap;
@@ -13,14 +11,13 @@ import java.util.Map;
 
 public class InventoryManager {
     public final Map<Player, Window> playerWindows = new HashMap<>();
-    @SuppressWarnings("unused")
-    private final msit plugin;
+    public final msit plugin;
     private final RandomIt randomIt;
 
     public InventoryManager(msit plugin) {
         this.plugin = plugin;
         this.randomIt = new RandomIt(plugin);
-        plugin.getServer().getPluginManager().registerEvents(new EventListener(this), plugin);
+        //plugin.getServer().getPluginManager().registerEvents(event, plugin);
     }
 
     public void openInventory(Player player) {
@@ -29,14 +26,12 @@ public class InventoryManager {
     }
 
     private Window createInventoryWindow(Player player) {
-        SimpleItem item = randomIt.getRandomItem();
         Gui gui = Gui.empty(9, 2);
 
         // set items using x and y coordinates
-        gui.setItem(0, 0, item);
-
-        // set an item using the slot index
-        gui.setItem(10, item);
+        gui.setItem(2, 0, randomIt.getRandomItem());
+        gui.setItem(4, 0, randomIt.getRandomItem());
+        gui.setItem(6, 0, randomIt.getRandomItem());
 
         Window window = Window.single()
         .setViewer(player)
@@ -44,19 +39,5 @@ public class InventoryManager {
         .setGui(gui)
         .build();
         return window;
-    }
-
-    public void ClickEvent(Player player, int slot) {
-        Gui gui = (Gui) playerWindows.get(player);
-        Item clickedItem = gui.getItem(slot);
-        if (clickedItem != null) {
-            // Add your logic here for when the inventories match and an item is clicked
-        }
-    }
-
-    public boolean isCorrectSlot(int slot) {
-        int x = slot % 9;
-        int y = slot / 9;
-        return (x == 3 || x == 5 || x == 7) && y == 2;
     }
 }
